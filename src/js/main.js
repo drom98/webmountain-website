2b9e20bad891e15ff6706b9c53c5fabc4d481c15
drom98 <diogoomarques98@gmail.com>
2019-04-26 16:50:56 +0100

    Add smooth-scroll

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 620
+  });
+
   const burgerBtn = document.querySelector('.burger');
   const navbarDataset = document.querySelector('#'+ burgerBtn.dataset.target);
   const navbar = document.querySelector('#navbarMenu');
@@ -7,24 +11,6 @@ document.addEventListener('DOMContentLoaded', () => {
     burgerBtn.classList.toggle('is-active');
     navbarDataset.classList.toggle('is-active');
   });
-
-  //Navbar animation
-  const mainNavEl = document.querySelector('#main-navbar');
-  const navItems = document.querySelectorAll('.main-nav-items');
-
-  function test(option) {
-    for(let i = 0; i < navItems[0].childElementCount; i++) {
-      for(let j = 0; j < navItems[0].children[i].classList.length; j++) {
-        if(navItems[0].children[i].classList[j] != 'is-active') {
-          if(option) {
-            navItems[0].children[i].classList[j].add('has-text-dark');
-          }
-        } else {
-          
-        }
-      }
-    }
-  }
   
   function setNavbarAnim() {
     const screenWidth = window.innerWidth;
